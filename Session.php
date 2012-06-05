a8e5c9fe5068dd2eeabde4382e2e06af12a9b8a6
Chason Choate <chason.choate@gmail.com>
2012-06-04 22:07:37 -0500

    Better debugging. Sessions now update the db properly.

--- Session.php
+++ Session.php
@@ -1,8 +1,13 @@
 <?php
+	function sessionLog($var) {
+		file_put_contents(__DIR__.'/session.log', $var." - ".time()."\n", FILE_APPEND);
+	}
 	// http://php.net/manual/en/session.customhandler.php
 	// http://www.php.net/manual/en/function.session-set-save-handler.php
 	class Session {
-		private $db;
+		private $db = NULL;
+		private $id = 0;
+		private $data = NULL;
 		function __construct($db = NULL) {
 			if (!empty($db)) {
 				$this->db = $db;
@@ -15,6 +20,13 @@
 					array($this, 'gc')
 				);
 				register_shutdown_function('session_write_close');
+				// Set cookie attributes
+				session_set_cookie_params(
+					0, 							// lifetime
+					'/', 						// path
+					$_SERVER['SERVER_NAME'], 	// domain
+					false 						// secure
+				);
 				// session_start() creates a session or resumes the current one based
 				// on a session identifier passed via a GET or POST request, or passed via a cookie.
 				session_start();
@@ -24,8 +36,9 @@
 		// The open callback works like a constructor in classes and is executed when the session
 		// is being opened. It is the first callback function executed when the session is started
 		// automatically or manually with session_start(). Return value is TRUE for success, FALSE for failure.
-		function open($savePath, $sessName) {
-			return true;
+		function open($savePath, $sessName) { 
+			sessionLog('Open');
+			return true; 
 		}
 
 		// The read callback must always return a session encoded (serialized) string, or an empty string
@@ -36,6 +49,8 @@
 		// to populate the $_SESSION superglobal. While the data looks similar to serialize() please note it is a
 		// different format which is speficied in the session.serialize_handler ini setting.
 		function read($sessId) {
+			announce('Read');
+			sessionLog('Read');
 			$this->db->select(
 				'*',
 				'php_session',
@@ -44,7 +59,13 @@
 			);
 			$sessions = $this->db->fetch_assoc_all();
 			debug($sessions);
-			return empty($sessions[0]) ? '' : $sessions[0];
+			if (count($sessions) === 1) { // Found a session
+				$this->id = $sessions[0]['session_id'];
+				$this->data = $sessions[0]['session_data'];
+				return $this->data;
+			} elseif (count($sessions) > 1) // Multiple session found
+				announce('Multiple Sessions Encountered.');
+			return  ''; // No session found
 		}
 
 		// The write callback is called when the session needs to be saved and closed. This callback receives the
@@ -55,21 +76,49 @@
 		// shuts down or explicitly when session_write_close() is called. Note that after executing this function PHP
 		// will internally execute the close callback.
 		function write($sessId, $encSessArray) {
+			announce('Write');
+			sessionLog('Write');
+			if ($this->id === 0) { // User does not have a session we know about
+				announce('Creating session');
+				$this->db->insert(
+					'php_session',
+					array(
+						'session_id' => $sessId,
+						'user_id' => NULL,
+						'date_created' => date('Y-m-d H:i:s'),
+						'last_updated' => date('Y-m-d H:i:s'),
+						'session_data' => $encSessArray
+					), false, true
+				);
+			} else { // Updating a user's session
+				announce('Updated session');
+				$this->db->update(
+					'php_session',
+					array(
+						'last_updated' => date('Y-m-d H:i:s'),
+						'session_data' => $encSessArray
+					),
+					'session_id = ?',
+					array($sessId), true
+				);
+			}
 			return true;
 		}
 
 		// The close callback works like a destructor in classes and is executed after the session write
 		// callback has been called. It is also invoked when session_write_close() is called. Return value
 		// should be TRUE for success, FALSE for failure.
-		function close() {
-			return true;
+		function close() { 
+			sessionLog('Close');
+			return true; 
 		}
 
 		// This callback is executed when a session is destroyed with session_destroy() or with 
 		// session_regenerate_id() with the destroy parameter set to TRUE. Return value should be TRUE for 
 		// success, FALSE for failure.
 		function destroy($sessId) {
-			return $this->db->delete("php_session", "session_id < ?", array($sessId), true);
+			sessionLog('Destroy');
+			return $this->db->delete("php_session", "session_id = ?", array($sessId), true);
 		}
 
 		// The garbage collector callback is invoked internally by PHP periodically in order to purge old session
@@ -77,14 +126,14 @@
 		// which is passed to this callback can be set in session.gc_maxlifetime. Return value should be TRUE for
 		// success, FALSE for failure.
 		function gc($maxLifetime) {
+			sessionLog('GC');
+			announce('Garbage');
 			$real_now = date('Y-m-d H:i:s');
 	        $dt1 = strtotime("$real_now -$maxLifetime seconds");
 	        $dt2 = date('Y-m-d H:i:s', $dt1);
 	        return $this->db->delete("php_session", "last_updated < ?", array($dt2), true);
 		}
 
-		function __destruct() {
-			@session_write_close();
-		}
+		function __destruct() { @session_write_close(); }
 	}
 ?>
--- index.php
+++ index.php
@@ -4,21 +4,34 @@
 		var_dump($var);
 		echo '</pre><br />';
 	}
-	require 'db/Zebra_Database.php';
+	function announce($var) {
+		echo '<h3>'.$var.'</h3>';
+	}
+
+	ob_start();
+	require __DIR__.'/db/Zebra_Database.php';
 	$db = new Zebra_Database();
 	$db->debug = true;
 	$db->connect('localhost', 'session', 's3ss10n', 'session');
 	$db->set_charset('utf8');
-	require 'Session.php';
+
+	require __DIR__.'/Session.php';
 	$session = new Session($db);
+	$_SESSION['custom'] = uniqid();
+	$data = ob_get_contents();
+	ob_end_clean();
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" href="/session/css/bootstrap.min.css" />
 		<title>Chason Session Test</title>
 	</head>
 	<body>
-		HI
-		<?php $db->show_debug_console(); ?>
+		<div class="container">
+			<?php echo $data; ?>
+			HTML Document
+			<?php $db->show_debug_console(); ?>
+		</div>
 	</body>
 </html>
